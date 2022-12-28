6c24f5c01abb74a269d08f6ffa652858031b17ff
ChathuraRubasingha <rubasingharcpk.19@uom.lk>
2022-12-28 13:39:33 +0530

    fourth Commit

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -8,7 +8,10 @@ const menuData = [ { title: "Courses", icon: "/images/icons/courses.svg", link: 
   icon: "/images/icons/tutorials.svg",
   link: "/tutorials",
 },
-{ title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },]
+{ title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
+{ title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
+{ title: "", icon: "/images/icons/search.svg", link: "/search" },
+{ title: "", icon: "/images/icons/account.svg", link: "/account" },]
 
 export default function Header() {
   return (
@@ -17,8 +20,10 @@ export default function Header() {
      <MenuWrapper>
 		{menuData.map(item => (
             <Link to={item.link}>
+            <MenuItem>
             <img src={item.icon} alt={item.title} />
             {item.title}
+            </MenuItem>
             </Link>
         ))}
     </MenuWrapper>
@@ -27,17 +32,17 @@ export default function Header() {
 }
 
 const Wrapper = styled.div`
-  position: absolute;
-  top: 60px;
-	grid-template-columns: 44px auto;
+    grid-template-columns: 44px auto;
 	justify-content: space-between;
+    position: absolute;
+    top: 60px;
 	align-items: center;
 	width: 100%;
 	padding: 0 30px;
 `
 const MenuWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: repeat(6, auto);
   gap: 30px;
 `
 const MenuItem = styled.div`
@@ -47,4 +52,12 @@ const MenuItem = styled.div`
   gap: 10px;
   align-items: center;
   padding: 10px;
+  border-radius: 10px;
+transition: 0.5s ease-out;
+
+:hover {
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
+    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+}
 `
